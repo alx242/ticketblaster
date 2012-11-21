485d48fd33660a32fbd3d83ceff8c15ccce96216
Alexander Schüssler <alexander.schussler@klarna.com>
2012-11-21 16:52:52 +0100

    More default values for base config stuff...bob seems like a nice name for stuff :)

--- src/ticketblaster.py
+++ src/ticketblaster.py
@@ -4,22 +4,24 @@ import sys
 import getopt
 import ticketblaster_bot
 
+def usage(Exit):
+  usage = "ticketblaster.py -s <server> -c <channel> -b <bot> -p <port>"
+  print usage
+  sys.exit(Exit)
+
 # Main initiatior of the ticketblaster bot
 def main(argv):
-  usage   = 'ticketblaster.py -s <server> -c <channel> -b <bot> -p <port>'
-  server  = ''
-  channel = ''
-  botnick = ''
+  server  = ""
+  channel = "bob"
+  botnick = "bob"
   port    = 6667
   try:
     opts, args = getopt.getopt(argv,"hs:c:b:p::",["server=","channel=","bot=","port="])
   except getopt.GetoptError:
-    print usage
-    sys.exit(2)
+    usage(2)
   for opt, arg in opts:
-    if opt == '-h':
-      print usage
-      sys.exit()
+    if opt == "-h":
+      usage(0)
     elif opt in ("-s", "--server"):
       server = arg
     elif opt in ("-c", "--channel"):
@@ -28,7 +30,8 @@ def main(argv):
       botnick = arg
     elif opt in ("-p", "--port"):
       port = arg
-  
+  if server == "":
+    usage(2)
   ticketblaster_bot.loop(server, int(port), "#"+channel, botnick)
 
 if __name__ == "__main__":
