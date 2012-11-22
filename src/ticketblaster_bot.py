0341035cc68a7adb989831b4d6d62ec5f4fa6d60
Alexander Schüssler <alexander.schussler@klarna.com>
2012-11-22 23:53:27 +0100

    Add the commands delete and grab to handle tickets. Also fixed the index handling

--- src/ticketblaster_bot.py
+++ src/ticketblaster_bot.py
@@ -18,25 +18,40 @@ def hello(ircsock, channel):
   msgs = ("Hello! I currently accept these commands:\n",
          "add <TICKET INFO>  - Add a new ticket\n",
          "show               - Display all current tickets\n",
+         "grab <TICKET ID>   - Assign ticket a owner\n",
          "delete <TICKET ID> - Delete a specific ticket (NOT IMPLEMENTED YET!)\n")
   for msg in msgs:
-    ircsock.send("PRIVMSG "+ channel +" :"+msg)
+    ircsock.send("PRIVMSG "+channel+" :"+msg)
 
 def add(ircsock, channel, info):
   ticketdb.add(info)
-  ircsock.send("PRIVMSG "+ channel +" :Added new ticket!\n")
+  ircsock.send("PRIVMSG "+channel+" :Added new ticket!\n")
 
 def show(ircsock, channel):
   tickets = ticketdb.getall()
+  ircsock.send("PRIVMSG "+channel+" :Current available tickets:\n")
   for ticket in tickets:
-    ircsock.send("PRIVMSG "+ channel +" : - " + ticket[1] + " \n")
+    ircsock.send("PRIVMSG "+channel+" : - "+str(ticket[0])+
+                 ": "+ticket[1].encode("utf-8")+"\n")
+
+def delete(ircsock, channel, id):
+  ticketdb.delete(id)
+  ircsock.send("PRIVMSG "+ channel +" : Removed ticket: "+id+" \n")
+
+def grab(ircsock, channel, owner, id):
+  ticketdb.grab(owner, id)
+  ircsock.send("PRIVMSG "+channel+" : Grabbed ticket: "+id+
+               ", go fix it!!! \n")
 
 # Connect and authenticate towards server
 def loop(server, port, channel, botnick):
   ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   ircsock.connect((server, port))
-  ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :BOOOOT!\n")
-  ircsock.send("NICK "+ botnick +"\n")
+  ircsock.send("USER "+botnick+
+               " "+botnick+
+               " "+botnick+
+               " :BOOOOT!\n")
+  ircsock.send("NICK "+botnick+"\n")
   joinchan(ircsock, channel) # Join the channel using the functions we previously defined
 
   # Bot loop
@@ -45,19 +60,29 @@ def loop(server, port, channel, botnick):
     ircmsg = ircmsg.strip('\n\r') # removing any unnecessary linebreaks.
     print(ircmsg) # Here we print what's coming from the server
 
-    # If we can find "Hello Mybot" it will call the function hello()
-    if ircmsg.upper().find(": HELLO") != -1:
-      hello(ircsock, channel)
-
     # If the server pings us then we've got to respond!
     if ircmsg.find("PING :") != -1:
       ping(ircsock)
 
+    # If we can find "Hello Mybot" it will call the function hello()
+    if ircmsg.upper().find(" HELLO") != -1:
+      hello(ircsock, channel)
+
     # Add a new ticket
-    if ircmsg.upper().find(": ADD") != -1:
-      add(ircsock, channel, ircmsg[ircmsg.upper().find(": ADD")+5:])
+    if ircmsg.upper().find(" ADD") != -1:
+      add(ircsock, channel, ircmsg[ircmsg.upper().find(" ADD")+4:])
 
     # Get all
-    if ircmsg.upper().find(": SHOW") != -1:
+    if ircmsg.upper().find(" SHOW") != -1:
       show(ircsock, channel)
+
+    # Delete a ticket
+    if ircmsg.upper().find(" DEL") != -1:
+      delete(ircsock, channel, ircmsg[ircmsg.upper().find(" DEL")+4:])
+
+    # Grab a ticket
+    if ircmsg.upper().find(" GRAB") != -1:
+      grab(ircsock, channel,
+           ircmsg[ircmsg.upper().find(":")+1:ircmsg.upper().find("!")],
+           ircmsg[ircmsg.upper().find(" GRAB")+5:])
 
