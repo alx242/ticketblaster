67668d1830fc4b856780028e0ae36c5494105372
alex <alex@zim.(none)>
2012-12-12 20:49:43 +0100

    Fixed the setup.py script for real distribution and installation, damn python is a breeze :D

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Handle ticketblaster packaging and distribution
 from setuptools import setup
 
 setup(name='ticketblaster',
       version='0.7',
-      description='Quick tickets blasted up using WSGI and a IRC-bot',
+      description='Quick tickets, blasted up using WSGI and a IRC-bot',
+      long_description = open('README').read(),
       url='http://github.com/alx242/ticketblaster',
       author='Alexander Schüssler',
       author_email='alex@xalx.net',
       license='MIT',
-      packages=['funniest'],
+      packages=['ticketblaster'],
+      scripts=['tblaster'],
       zip_safe=False)
